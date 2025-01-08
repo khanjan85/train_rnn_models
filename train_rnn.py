aba67c0f2438549290e42add63f749a72d15d2c0
chirag <61842354+chiragpalan@users.noreply.github.com>
2025-01-08 18:50:32 +0530

    Update train_rnn.py

--- train_rnn.py
+++ train_rnn.py
@@ -60,7 +60,7 @@ def train_model(table_name):
     print("model path", model_path)
     if os.path.exists(model_path):
         os.remove(model_path)
-    # model.save(model_path)
+    model.save(model_path)
     file_path = f"{MODELS_FOLDER}/{table_name}_scaler.pkl"
     print(f"Saving scaler to: {file_path}")
     if os.path.exists(file_path):
