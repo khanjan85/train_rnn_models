88af335395be533931aec04e37a4cd9886ed8256
chirag <61842354+chiragpalan@users.noreply.github.com>
2025-01-08 15:46:12 +0530

    Update train_rnn.py

--- train_rnn.py
+++ train_rnn.py
@@ -1,6 +1,7 @@
 import os
 import sqlite3
 import pandas as pd
+import numpy as np
 from sklearn.preprocessing import MinMaxScaler
 from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import LSTM, Dense
